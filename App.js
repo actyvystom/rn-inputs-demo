5cf496e3ae0729a70e1335ae458fa97fff6740e2
actyvystom <thomas.goerldt@actyvyst.com>
2025-02-26 11:04:49 +0100

    demo finished

--- App.js
+++ App.js
@@ -1,10 +1,52 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
-
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  Button,
+  Pressable
+} from "react-native";
+import { useState } from "react";
 export default function App() {
+  const [name, setName] = useState("");
+  const [password, setPassword] = useState("");
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
+      <Text>Login</Text>
+      <TextInput
+        placeholder="User name"
+        onChangeText={(text) => {
+          setName(text);
+        }}
+        value={name}
+      />
+      <TextInput
+        placeholder="password"
+        onChangeText={(pw) => {
+          setPassword(pw);
+        }}
+        value={password}
+        secureTextEntry
+      />
+      {/* <Button
+        onPress={() => {
+          console.log("Username: ", name);
+          console.log("Password: ", password);
+        }}
+        title="Login"
+      /> */}
+      <Pressable
+        onPress={() => {
+          console.log("Username: ", name);
+          console.log("Password: ", password);
+        }}
+      >
+        <View style={styles.button}>
+          <Text style={{ color: "white" }}>Login</Text>
+        </View>
+      </Pressable>
     </View>
   );
 }
@@ -15,5 +57,14 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center"
+  },
+  button: {
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 10,
+    width: 200,
+    height: 40,
+    backgroundColor: "blue",
+    borderRadius: 4
   }
 });
